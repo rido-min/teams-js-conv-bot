005ca635eab72e58f573cade8aefaef11a339ef4
rido <rmpablos@microsoft.com>
2024-11-27 18:23:28 -0800

    start

---
+++ app.js
@@ -0,0 +1,40 @@
+// ts-check
+const express = require('express')
+
+const {
+    CloudAdapter, ConfigurationBotFrameworkAuthentication,
+} = require('botbuilder')
+
+const { TeamsConversationBot } = require('./bot')
+
+
+const adapter = new CloudAdapter(new ConfigurationBotFrameworkAuthentication(process.env));
+const myBot = new TeamsConversationBot()
+
+const server = express()
+server.use(express.json())
+
+server.post('/api/messages', 
+    async (req, res) => {
+        console.log(req.body)
+        await adapter.process(req, res, (context) => myBot.run(context));
+    }
+)
+
+const port = process.env.PORT || 3978
+
+server.listen(port, () => {
+    console.log(`\n lisenting on ${ port } for bot ${ process.env.MicrosoftAppId }`);
+})
+
+process.on('SIGTERM', () => {
+    server.close(() => {
+        console.log('Process terminated')
+    })
+})
+
+process.on('SIGINT', () => {
+    server.close(() => {
+        console.log('Process interrupted')
+    })
+})
